c5758fc24813b573151486e886ab978b6c9600ad
Javier Jimenez <javierrcc522@gmail.com>
2017-10-22 12:39:58 -0700

    add class task, and constructor

--- app/to-do.ts
+++ app/to-do.ts
@@ -1,11 +1,18 @@
 class Task {
   done: boolean = false;
+  //we dont have to use this properties
+  // description: string;
+  // priority: string;
 
-  constructor(public description: string, public priority: string){}
+  constructor(public description: string, public priority: string)
+  {
+    // this.done = false; //delete to use in properties
+    // this.description = descriptionParameter;
+    // this.priority = priorityParameter;
+
+  }
 }
 
 var tasks: Task[] = [];
 tasks.push(new Task('Do the dishes.', 'Medium'));
-tasks.push(new Task('Buy chocolate.', 'Low'));
-tasks.push(new Task('Do laundry.', 'High'));
 console.log(tasks);
